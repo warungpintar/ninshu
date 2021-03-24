bfffeb89b329219ac9946eaccd63c6b7940de865
adeyahya <adeyahyaprasetyo@gmail.com>
2021-03-24 17:48:56 +0700

    add test cases

--- __tests__/is/fqdn.spec.ts
+++ __tests__/is/fqdn.spec.ts
@@ -9,6 +9,8 @@ describe("postive cases", () => {
 describe("negative cases", () => {
   test("it should fail", () => {
     expect(isFqdn("f!oo.com")).toBeFalsy();
+    expect(isFqdn("foo")).toBeFalsy();
+    expect(isFqdn("foo_.bar._foo")).toBeFalsy();
     expect(
       isFqdn(
         "www.badexample.com/accessToken=eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJlNjc4ZjIzMzQ3ZTM0MTBkYjdlNjg3Njc4MjNiMmQ3MCIsImlhdCI6MTQ2NjYzMzMxNywibmJmIjoxNDY2NjMzMzE3LCJleHAiOjE0NjY2MzY5MTd9.rgx_o8VQGuDa2AqCHSgVOD5G68"
